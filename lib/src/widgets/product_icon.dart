bd2af08c7db3c0f605c6a6060c5c369a654412bf
Finn Jesse Dan <83787516+Jesse-Dan@users.noreply.github.com>
2023-10-01 10:55:43 +0100

    final build after forking

--- lib/src/widgets/product_icon.dart
+++ lib/src/widgets/product_icon.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unnecessary_null_comparison
+
 import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce_app/src/model/category.dart';
 import 'package:flutter_ecommerce_app/src/themes/light_color.dart';
